3f9272231831581b70285abc44cde9b280ff0abe
cleetus-j <82292256+cleetus-j@users.noreply.github.com>
2024-02-27 23:17:25 +0100

    Add files via upload

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ wla-z80 -o 'HOTL.o' 'HOTL_mod.asm'
 echo [objects] > linkfile
 echo 'HOTL.o' >>linkfile
 wlalink linkfile HOTL_mod.sms
-cp HOTL_mod.sms /media/work/devver/disas/HOTL/smsexamine
+ #cp HOTL_mod.sms /media/work/devver/disas/HOTL/smsexamine
 
